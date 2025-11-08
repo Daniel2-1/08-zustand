4e7cb7a9421e780b60c41722eff57b1378faf0b6
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2025-11-08 10:16:02 +0100

    new

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,6 +1,25 @@
 "use client";
 import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./CreateNote.module.css";
+import { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Create-Note-Page",
+  description: "Now you can create your own note.",
+  openGraph: {
+    title: "Create-Note-Page",
+    description: "Open to create your own note",
+    url: "https://08-zustand-tau-ten.vercel.app/notes/action/create",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Note Image",
+      },
+    ],
+  },
+};
 
 export default function CreateNote() {
   return (
